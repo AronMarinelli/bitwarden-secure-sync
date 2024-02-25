80d4d826285dcfab2a045cc0f80486d4833fd652
Aron Marinelli <aron@marinelli.nl>
2024-02-25 21:32:51 +0100

    Minor changes to startup process

--- Bitwarden.SecureSync.Application/Program.cs
+++ Bitwarden.SecureSync.Application/Program.cs
@@ -9,57 +9,53 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-var builder = Host.CreateApplicationBuilder(args);
+{
+    var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.AddLogging(
-    loggingBuilder =>
-    {
-        loggingBuilder.AddFilter("Microsoft", LogLevel.Warning)
-            .AddFilter("System", LogLevel.Warning)
-            .AddConsole();
-    });
+    builder.Services.AddLogging(
+        loggingBuilder =>
+        {
+            loggingBuilder.AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddConsole();
+        });
 
-Console.WriteLine("Starting Bitwarden Secure Sync tool...");
+    Console.WriteLine("Starting Bitwarden Secure Sync tool...");
 
-var configurationRoot = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional:true, reloadOnChange:true)  
-    .AddEnvironmentVariables(prefix: "BWSYNC_")           
-    .Build();
+    var configurationRoot = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables(prefix: "BWSYNC:")
+        .Build();
 
-configurationRoot["Logging:LogLevel:Microsoft"] = "None";
+    InjectConfiguration(builder.Services, configurationRoot);
 
-InjectConfiguration(builder.Services, configurationRoot);
+    builder.Services.AddHttpClient();
 
-builder.Services.AddHttpClient();
+    builder.Services.AddSingleton<IBitwardenClientFactory, BitwardenClientFactory>();
 
-builder.Services.AddSingleton<IBitwardenClientFactory, BitwardenClientFactory>();
+    builder.Services.AddTransient<IBitwardenClientDownloadLogic, BitwardenClientDownloadLogic>();
+    builder.Services.AddTransient<ISynchronisationLogic, SynchronisationLogic>();
 
-builder.Services.AddTransient<IBitwardenClientDownloadLogic, BitwardenClientDownloadLogic>();
-builder.Services.AddTransient<ISynchronisationLogic, SynchronisationLogic>();
+    builder.Services.AddHostedService<ApplicationHost>();
 
-builder.Services.AddHostedService<ApplicationHost>();
+    using var host = builder.Build();
 
-using var host = builder.Build();
-
-await host.RunAsync();
+    await host.RunAsync();
+}
 return;
 
-void InjectConfiguration(IServiceCollection services, IConfiguration configuration)
+static void InjectConfiguration(IServiceCollection services, IConfiguration configuration)
 {
-    var localSettingsAvailable = File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"));
-    
     var bitwardenConfiguration = new BitwardenConfiguration();
     var syncConfiguration = new SyncConfiguration();
-    if (localSettingsAvailable)
-    {
-        configuration.GetSection("Bitwarden").Bind(bitwardenConfiguration);
-        configuration.GetSection("Sync").Bind(syncConfiguration);
-    }
+    
+    configuration.GetSection("Bitwarden").Bind(bitwardenConfiguration);
+    configuration.GetSection("Sync").Bind(syncConfiguration);
 
     bitwardenConfiguration.Validate();
     syncConfiguration.Validate();
 
-    builder.Services.AddSingleton(bitwardenConfiguration);
-    builder.Services.AddSingleton(syncConfiguration);
+    services.AddSingleton(bitwardenConfiguration);
+    services.AddSingleton(syncConfiguration);
 }
