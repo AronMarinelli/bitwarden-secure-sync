b93b090746e551af932c3b6d79e08e368f5223fa
Aron Marinelli <aron@marinelli.nl>
2024-05-06 19:43:57 +0200

    Bump Bitwarden client version to 2024.4.0

--- Bitwarden.SecureSync.Logic/Client/BitwardenClientDownloadLogic.cs
+++ Bitwarden.SecureSync.Logic/Client/BitwardenClientDownloadLogic.cs
@@ -6,18 +6,18 @@ namespace Bitwarden.SecureSync.Logic.Client;
 
 public class BitwardenClientDownloadLogic : IBitwardenClientDownloadLogic
 {
-    private const string CLIENT_VERSION_WINDOWS = "v2024.2.1";
-    private const string CLIENT_VERSION_OSX = "v2024.3.1";
-    private const string CLIENT_VERSION_LINUX = "v2024.3.1";
+    private const string CLIENT_VERSION_WINDOWS = "v2024.4.0";
+    private const string CLIENT_VERSION_OSX = "v2024.4.0";
+    private const string CLIENT_VERSION_LINUX = "v2024.4.0";
 
     private const string WINDOWS_CLIENT_URL =
-        "https://github.com/bitwarden/clients/releases/download/cli-v2024.2.1/bw-windows-2024.2.1.zip";
+        "https://github.com/bitwarden/clients/releases/download/cli-v2024.4.0/bw-windows-2024.4.0.zip";
 
     private const string LINUX_CLIENT_URL =
-        "https://github.com/bitwarden/clients/releases/download/cli-v2024.3.1/bw-linux-2024.3.1.zip";
+        "https://github.com/bitwarden/clients/releases/download/cli-v2024.4.0/bw-linux-2024.4.0.zip";
 
     private const string OSX_CLIENT_URL =
-        "https://github.com/bitwarden/clients/releases/download/cli-v2024.3.1/bw-macos-2024.3.1.zip";
+        "https://github.com/bitwarden/clients/releases/download/cli-v2024.4.0/bw-macos-2024.4.0.zip";
 
     private readonly DirectoryInfo _clientDownloadDirectory;
     private readonly FileInfo _clientFile;
