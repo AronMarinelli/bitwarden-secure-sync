ad6ded1883ec8f502c7b7ff70d4b582562163b0a
Aron Marinelli <aron@marinelli.nl>
2024-08-04 15:45:30 +0200

    Update docker entrypoint in order to resolve possible permission issues with Bitwarden .cache directory

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir /.cache && chmod -R 777 /.cache
+chown -R ${PUID}:${PGID} /.cache
+
 mkdir /.config && mkdir /.config/Bitwarden\ CLI
 chown -R ${PUID}:${PGID} /.config/Bitwarden\ CLI
 
